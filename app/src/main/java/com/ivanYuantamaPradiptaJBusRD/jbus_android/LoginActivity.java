4e109f953462e212f2d6bd02949bdd3d2bd45555
Ivan Yuantama <ivanyp59@gmail.com>
2023-11-17 11:40:05 +0700

    CS8 - Menyelesaikan Case Study

--- app/src/main/java/com/ivanYuantamaPradiptaJBusRD/jbus_android/LoginActivity.java
+++ app/src/main/java/com/ivanYuantamaPradiptaJBusRD/jbus_android/LoginActivity.java
@@ -2,13 +2,44 @@ package com.ivanYuantamaPradiptaJBusRD.jbus_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView registerNow = null;
+    private Button loginButton = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        registerNow = findViewById(R.id.text_register);
+        loginButton = findViewById(R.id.login_button);
+
+        registerNow.setOnClickListener(v -> {
+            moveActivity(this, RegisterActivity.class);
+        });
+
+        loginButton.setOnClickListener(v -> {
+            viewToast(this, "This is for login");
+            moveActivity(this, MainActivity.class);
+        });
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+    }
+
+    private void moveActivity(Context ctx, Class<?> cls){
+        Intent intent = new Intent(ctx, cls);
+        startActivity(intent);
+    }
+
+    private void viewToast(Context ctx, String message){
+        Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/ivanYuantamaPradiptaJBusRD/jbus_android/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.ivanYuantamaPradiptaJBusRD.jbus_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+    }
+}
