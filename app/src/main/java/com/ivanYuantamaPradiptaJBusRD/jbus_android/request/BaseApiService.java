bd01443bd1dc3aec7243bd0b3672227a567fffec
Ivan Yuantama <ivanyp59@gmail.com>
2023-11-23 19:01:51 +0700

    TP9 - Menyelesaikan Tugas Pendahuluan

---
+++ app/src/main/java/com/ivanYuantamaPradiptaJBusRD/jbus_android/request/BaseApiService.java
@@ -0,0 +1,15 @@
+package com.ivanYuantamaPradiptaJBusRD.jbus_android.request;
+
+import com.ivanYuantamaPradiptaJBusRD.jbus_android.model.Account;
+import com.ivanYuantamaPradiptaJBusRD.jbus_android.model.BaseResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface BaseApiService {
+    @GET("account/{id}")
+    Call<Account> getAccountbyId (@Path("id") int id);
+}
+
